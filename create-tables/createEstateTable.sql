117a4e0df400d3dca482fb1135cdad62c0a870d6
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-05 15:50:23 -0300

    Creating state table

---
+++ create-tables/createEstateTable.sql
@@ -0,0 +1,11 @@
+-- CREATING STATE TABLE
+CREATE TABLE estate (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    region ENUM('North', 'Northeast', 'Midwest', 'Southeast', 'South') NOT NULL,
+    population DECIMAL(5,2) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE KEY (name),
+    UNIQUE KEY (uf)
+)
