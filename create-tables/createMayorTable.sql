08b7024e8331970be26cc4d1371f5a7b4c88701a
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-05 23:43:56 -0300

    Creating mayors table

---
+++ create-tables/createMayorTable.sql
@@ -0,0 +1,9 @@
+-- CREATING CITIES TABLE
+CREATE TABLE IF NOT EXISTS mayors (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    cities_id INT UNSIGNED,
+    PRIMARY KEY(id),
+    UNIQUE KEY (cities_id),
+    FOREIGN KEY(cities_id) REFERENCES cities (id)
+);
